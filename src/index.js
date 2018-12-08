ba9218fccdf6b6bfbd77a6299a5fd18da61ebf97
Ernesto Jiménez <ernesto@bedu.org>
2018-12-08 11:50:09 -0600

    Example finished

--- src/index.js
+++ src/index.js
@@ -1,15 +1,35 @@
-class BitchiButton{
-	constructor(selector){
-		this.el = document.querySelector(selector);
-		console.log(this.el);
-		this.el.addEventListener('click', this.complain);
-	}
 
-	complain(){
-		console.log('Ouch!!')
-	}
+class App{
+  constructor(selector){
+    this.el = document.querySelector(selector);
+    this.list = this.el.querySelector('ul');
+    this.init();
+  }
+
+  init(){
+    fetch('http://localhost:3000/authors')
+      .then(request => request.json())
+      .then(authors => {
+        this.authors = authors;
+        this.render();
+      });
+  }
+
+  render(){
+    this.authors.forEach(author => {
+      // crear un <li>.
+      var li = document.createElement('li');
+      // agregar al <li> el nombre del autor ().
+      li.textContent = author.name
+      // agregar el <li> a la lista.
+      this.list.appendChild(li);
+    })
+  }
 }
 
-var bb1 = new BitchiButton('#bitchiButton1');
-var bb2 = new BitchiButton('#bitchiButton2');
-var bb3 = new BitchiButton('#bitchiButton3');
+var myApp = new App('#app');
+
+// PascalCase
+// cammelCase
+// kebab-case
+// snake_case
